0d0ff14264155c3c1453f78e1752aa74e0572bbf
ckayakman <calimass05@hotmail.com>
2025-12-13 12:52:03 -0700

    GitHub ready

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/jazz-chords/',
     server: {
         host: '127.0.0.1',
     },
